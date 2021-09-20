d71cbd3eaafc7b0fc2e8729281db2183c27345ad
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-09-20 15:40:55 +0300

    10

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const seqelize = require('./db')
 const PORT = process.env.PORT || 5001
 
 
 const app = express()
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const start = async () => {
+    try {
+        await seqelize.authenticate()
+        await seqelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+start()
+////
