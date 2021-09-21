da08f360cb701a0a23f3b477a63ef139c6fb4eb1
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-09-21 09:47:53 +0300

    25

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ require('dotenv').config()
 const express = require('express')
 const seqelize = require('./db')
 const models =require ('./models/models')
+const cors = require ('cors')
+
 const PORT = process.env.PORT || 5001
 
  
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (reg, res) => {
+    res.status(200).json({message:'WORKING!!!'})
+})
 
 const start = async () => {
     try {
