92aa45dde7e56bac16eee08ef5cba5e49ee82ecc
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-09-22 18:42:53 +0300

    221842

--- client/src/Components/Autorization/Autorization.jsx
+++ client/src/Components/Autorization/Autorization.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import s from './Autorization.module.css'
 import { NavLink } from 'react-router-dom'
-
+import signInPhoto from '../../assets/images/SingInWithGoogle.png'
 
 // import Outh from './../Outh/Outh';
 
 const Autorization = (props) => {
     return (
-        <header className={s.header}>
-            <img src='https://kpi.ua/files/kpi_0.png' />
+        <header className={s.autorization}>
+            
+<div>Для начала нужно авторизироваться</div>
+
             <div className={s.loginBlock}>
-                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                {props.isAuth ? props.login : <NavLink to={'/login'}><img src={signInPhoto} /></NavLink>}
 
 
             </div>
