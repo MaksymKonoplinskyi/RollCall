0ff3f370d882a74aaee780fb15fc4fb403f46845
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-09-22 15:52:28 +0300

    auth finish

--- server/index.js
+++ server/index.js
@@ -9,15 +9,72 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5001
 
+const bodyParser = require('body-parser')
+const passport = require('passport');
+const cookieSession = require('cookie-session')
+require('./passport-setup');
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// For an actual app you should configure this with an experation time, better keys, proxy and secure
+app.use(cookieSession({
+    name: 'tuto-session',
+    keys: ['key1', 'key2']
+  }))
+
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Обработка шибок в конце
 app.use(errorHandler)
+
+// Example protected and unprotected routes
+app.get('/', (req, res) => res.send('Example Home page!'))
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  }
+);
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
+
+//app.listen(5000, () => console.log(`Example app listening on port ${5000}!`))
+
+
+
+
 
 const start = async () => {
     try {
@@ -29,4 +86,4 @@ const start = async () => {
     }
 }
 start()
-////
+//// https://www.youtube.com/watch?v=o9e3ex-axzA  гугл авторизация  
