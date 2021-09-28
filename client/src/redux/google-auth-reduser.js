0af53df31b138421568c2975bfc57a90e21b90c2
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-09-28 16:07:40 +0300

    3

--- client/src/redux/google-auth-reduser.js
+++ client/src/redux/google-auth-reduser.js
@@ -1,5 +1,5 @@
 const SET_USER_G_DATA = 'SET_USER_G_DATA';
-
+const SET_USER_DATA = 'SET_USER_DATA';
 
 
 let initialState = {
@@ -19,16 +19,20 @@ const googleAuthReduser = (state = initialState, action) => {
                 ...state,
                 ...action.data,
                 isAuth: true,
-
             };
+        case SET_USER_DATA:
 
-
+            return {
+                ...state,
+                ...action.data,
+                isAuth: true,
+            };
 
         default:
             return state
     }
 }
 export const setUserGData = (id, email) => ({ type: SET_USER_G_DATA, data: { id, email } });
-
+export const setUserData = (id, email) => ({ type: SET_USER_DATA, data: { id, email } });
 
 export default googleAuthReduser
