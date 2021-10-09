ff877251b75272d00ddcdae2534fec9144d4144c
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-10-09 23:16:08 +0300

    66

--- client/src/Components/Outh/Outh.js
+++ client/src/Components/Outh/Outh.js
@@ -1,91 +1,101 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getTeacherData } from '../../http/userAPI';
 import './Outh.css';
 // import { useHistory } from 'react-router';
 
-class Outh extends React.Component {
-// const history = useHistory()
-  
-  componentDidMount() {
-    window.gapi.load('auth2', function () {
-      window.gapi.auth2
-        .init({
-          clint_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        })
-        .then(() => { }, () => console.log('init ERR'));
-    });
+const Outh = (props) => {
+  // const history = useHistory()
+  useEffect(() => {
+    
+      window.gapi.load('auth2', function () {
+        window.gapi.auth2
+          .init({
+            clint_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          })
+          .then(() => { }, () => console.log('init ERR'));
+      })
+   
+  }, [])
+// componentDidMount() {
+//   window.gapi.load('auth2', function () {
+//     window.gapi.auth2
+//       .init({
+//         clint_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+//       })
+//       .then(() => { }, () => console.log('init ERR'));
+//   });
+// }
+
+
+
+const signIn = () => {
+  const _authOk = googleUser => {
+
+    const gUser = {
+      g_id: googleUser.getBasicProfile().getId(),
+      email: googleUser.getBasicProfile().getEmail(),
+      g_name: googleUser.getBasicProfile().getName(),
+      avaUrl: googleUser.getBasicProfile().getImageUrl(),
+      id_token: googleUser.getAuthResponse().id_token,
+    }
+    props.setUserGData(gUser)
+
+
+    const trySetTeacherData = async () => {
+      try {
+        const responseData = await getTeacherData(gUser.g_id)
+        const userData = { ...responseData.data }
+        props.setUserData(userData)
+        console.log(responseData);
+        console.log(responseData.data.id);
+
+        // history.push(`/teacher/${response.data.id}`)
+      } catch (e) {
+        alert(e.response.data.message)
+      }
+    }
+    trySetTeacherData()
+
   }
 
-  
- 
-  signIn = () => {
-    const _authOk = googleUser => {
 
+
+  const _authErr = () => console.log('Auth Err')
+
+
+  const GooqleAuth = window.gapi.auth2.getAuthInstance()
+
+  GooqleAuth.signIn({
+    scope: 'profile email'
+  }).then(_authOk, _authErr)
+}
+
+const signOut = () => {
+  const GooqleAuth = window.gapi.auth2.getAuthInstance()
+  GooqleAuth.signOut().then(() => {
+
+    if (true) {
       const gUser = {
-        g_id: googleUser.getBasicProfile().getId(),
-        email: googleUser.getBasicProfile().getEmail(),
-        g_name: googleUser.getBasicProfile().getName(),
-        avaUrl: googleUser.getBasicProfile().getImageUrl(),
-        id_token: googleUser.getAuthResponse().id_token,
+        g_id: null,
+        email: null,
+        g_name: null,
+        avaUrl: null,
+        id_token: null,
       }
-      this.props.setUserGData(gUser)
-     
-
-      const trySetTeacherData = async () => {
-        try {
-          const responseData = await getTeacherData(gUser.g_id)
-          const userData = { ...responseData.data }
-          this.props.setUserData(userData)
-          console.log(responseData);
-          console.log(responseData.data.id);
-          
-          // history.push(`/teacher/${response.data.id}`)
-        } catch (e) {
-          alert(e.response.data.message)
-        }
-      }
-      trySetTeacherData()
-
+      props.setUserGData(gUser.g_id, gUser.email, gUser.g_name, gUser.avaUrl, gUser.id_token)
     }
 
-
-
-    const _authErr = () => console.log('Auth Err')
-
-
-    const GooqleAuth = window.gapi.auth2.getAuthInstance()
-
-    GooqleAuth.signIn({
-      scope: 'profile email'
-    }).then(_authOk, _authErr)
-  }
-
-  signOut = () => {
-    const GooqleAuth = window.gapi.auth2.getAuthInstance()
-    GooqleAuth.signOut().then(() => {
-
-      if (true) {
-        const gUser = {
-          g_id: null,
-          email: null,
-          g_name: null,
-          avaUrl: null,
-          id_token: null,
-        }
-        this.props.setUserGData(gUser.g_id, gUser.email, gUser.g_name, gUser.avaUrl, gUser.id_token)
-      }
-
-    }, () => console.log('signOut ERR'))
-  }
-  render() {
-    const { g_id } = this.props.gUser
-    return (
-      <div className="Outh">
-        {!g_id && <button onClick={this.signIn} >Log In</button>}
-        {!!g_id && <button onClick={this.signOut} >Log Out</button>}
-      </div>
-    )
-  }
+  }, () => console.log('signOut ERR'))
 }
+
+  const { g_id } = props.gUser
+  return (
+    <div className="Outh">
+      {!g_id && <button onClick={signIn} >Log In</button>}
+      {!!g_id && <button onClick={signOut} >Log Out</button>}
+    </div>
+  )
+}
+
 
 export default Outh;
--- client/src/Components/RegTeacher/RegTeacherContainer.jsx
+++ client/src/Components/RegTeacher/RegTeacherContainer.jsx
@@ -4,11 +4,11 @@ import RegTeacher from './RegTeacher';
 
 const mapStateToProps = (state) => {
     return {
-        g_id: state.auth.g_id,
-        g_name: state.auth.g_name,
-        email: state.auth.email,
-        id_token: state.auth.id_token
-        // messages: state.dialogsPage.messages,
+        g_id: state.auth.gUser.g_id,
+        g_name: state.auth.gUser.g_name,
+        email: state.auth.gUser.email,
+        id_token: state.auth.gUser.id_token
+        
 
     }
 }
