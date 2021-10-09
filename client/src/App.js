79d95adc6ed56673597f019d28e06799465031a4
MaksymKonoplinskyi <gran7777777@gmail.com>
2021-10-09 21:58:22 +0300

    333

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,9 @@ import './App.css'
 import NavBar from './Components/NavBar/NavBar';
 import ProfileContainer from './Components/Profile/ProfileContainer';
 import { Route } from 'react-router-dom';
-import Settings from './Components/Settings/Settings';
-import Music from './Components/Music/Music';
-import News from './Components/News/News';
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
 import UsersContainer from './Components/Users/UsersContainer';
 import HeaderContainer from './Components/Header/HeaderContainer';
-import AutorizationContainer from './Components/Autorization/AutorizationContainer';
 import RegStudentContainer from './Components/RegStudent/RegStudentContainer';
 import AuthContainer from './Components/Auth/AuthContainer';
 import RegTeacherContainer from './Components/RegTeacher/RegTeacherContainer';
@@ -33,11 +29,7 @@ const App = (props) => {
         <Route path='/dialogs' render={() => <DialogsContainer />} />
         <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
         <Route path='/users' render={() => <UsersContainer />} />
-        <Route path='/autorization' render={() => <AutorizationContainer />} />
 
-        <Route path='/News' render={() => <News />} />
-        <Route path='/Music' render={() => <Music />} />
-        <Route path='/Settings' render={() => <Settings />} />
       </div>
     </div>
 
